5399bdd76585be8aef56a1f9b9df152290c0f98e
Kieran Burke <kieran314237@hotmail.co.uk>
2022-06-20 02:21:12 +0900

    chore: comment out some null-ls

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -9,21 +9,26 @@ end
 null_ls.setup({
   debug = true,
   sources = {
-
-    null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.diagnostics.pyright,
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.formatting.eslint_d,
-    null_ls.builtins.diagnostics.markdownlint,
-    null_ls.builtins.formatting.shfmt,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.diagnostics.markdownlint,
-    null_ls.builtins.code_actions.refactoring,
     null_ls.builtins.formatting.prettierd.with({
       env = {
         PRETTIERD_LOCAL_PRETTIER_ONLY = 1,
       },
     }),
+    null_ls.builtins.formatting.trim_whitespace.with({
+      filetypes = { "plantuml" },
+    }),
+    -- null_ls.builtins.formatting.stylua,
+    null_ls.builtins.code_actions.eslint_d,
+    -- null_ls.builtins.diagnostics.pyright,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.diagnostics.prettierd,
+    null_ls.builtins.formatting.eslint_d,
+    -- null_ls.builtins.diagnostics.markdownlint,
+    -- null_ls.builtins.formatting.shfmt,
+    -- null_ls.builtins.diagnostics.shellcheck,
+    -- null_ls.builtins.code_actions.shellcheck,
+    -- null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.code_actions.refactoring,
     -- null_ls.builtins.formatting.stylua,
     null_ls.builtins.code_actions.gitsigns,
     -- diagnostics.flake8
